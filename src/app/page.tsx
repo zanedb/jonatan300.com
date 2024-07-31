88abca062384cbbfe12652feb249c3496857c457
Zane <github@zane.sh>
2024-07-30 18:37:19 -0700

    add commission

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+import Nav from '@/components/nav'
 import Gallery from '@/components/gallery'
 import { newSpirit } from './fonts'
 
 export default function Home() {
   return (
-    <main className="p-8 pt-0 md:p-12 max-w-screen-lg w-full mx-auto">
-      <h1 className={`text-xl pb-4 ${newSpirit.className} font-medium`}>
-        hi, i'm ***REMOVED***.
-      </h1>
-      <p>
-        i'm an artist based in sf, currently studying studio art at san
-        ***REMOVED***.
-      </p>
+    <main className="flex flex-col sm:flex-row">
+      <div className="flex-none sm:w-[20em] md:w-[23em] md:pl-4 top-0 sm:h-screen sm:sticky overflow-x-hidden sm:overflow-y-auto no-scrollbar">
+        <div className="space-y-4 p-4">
+          <Nav />
+          <h1 className={`text-2xl ${newSpirit.className} font-medium`}>
+            hi, i'm ***REMOVED***.
+          </h1>
+          <p>
+            i'm an artist based in sf, currently studying studio art at san
+            ***REMOVED***.
+          </p>
+          <p>
+            i'm available for commission. preferably spray painted murals,
+            digital art (including artist/album covers), and digital
+            illustration. contact me below if interested.
+          </p>
+        </div>
+      </div>
       <Gallery />
     </main>
   )
