176040cb9ff7d41eb2ade26c21a9c8e0d0fc0e3e
Zane <github@zane.sh>
2024-08-10 19:09:57 -0700

    light/dark mode logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,23 +1,18 @@
 import Link from 'next/link'
-import Image from 'next/image'
-import { newSpirit } from '@/app/fonts'
+import { ThemeImage } from './theme-image'
 
 export default function Nav() {
   return (
     <nav className="flex justify-between items-center text-md py-4">
       <Link href="/">
-        <Image src="/nameplate.png" alt="***REMOVED***" width={72} height={0} />
+        <ThemeImage
+          srcLight="/nameplate.png"
+          srcDark="/nameplate-dark.png"
+          alt="nametag with swag"
+          width={72}
+          height={0}
+        />
       </Link>
-      <ul className="">
-        <li>
-          <Link
-            href="/contact"
-            className={`font-medium ${newSpirit.className}`}
-          >
-            contact
-          </Link>
-        </li>
-      </ul>
     </nav>
   )
 }
