3e43f7f332c8534e392697db17627105652b1b3d
Zane <github@zane.sh>
2024-08-12 14:50:23 -0700

    Opengraph fix?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,6 @@ import './globals.css'
 export const metadata: Metadata = {
   title: '***REMOVED*** web site',
   description: 'i’m an artist and student based in san francisco, ca.',
-  openGraph: {
-    images: '/og-image.png',
-  },
 }
 
 export default function RootLayout({
