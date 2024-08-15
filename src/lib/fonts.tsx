90e8a5f121fbfa790df5232a809ff92d45d5d7c6
Zane <github@zane.sh>
2024-08-15 14:41:26 -0700

    Remove unused font files

--- src/lib/fonts.tsx
+++ src/lib/fonts.tsx
@@ -3,11 +3,6 @@ import localFont from 'next/font/local'
 export const newSpirit = localFont({
   src: [
     {
-      path: '../../public/fonts/NewSpirit-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
       path: '../../public/fonts/NewSpirit-Regular.woff2',
       weight: '400',
       style: 'normal',
@@ -15,16 +10,6 @@ export const newSpirit = localFont({
     {
       path: '../../public/fonts/NewSpirit-Medium.woff2',
       weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/NewSpirit-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/NewSpirit-Bold.woff2',
-      weight: '700',
       style: 'normal',
     },
   ],
@@ -36,16 +21,6 @@ export const rebondGrotesque = localFont({
     {
       path: '../../public/fonts/ESRebondGrotesque-Regular.woff2',
       weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/ESRebondGrotesque-Italic.woff2',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../../public/fonts/ESRebondGrotesque-Bold.woff2',
-      weight: '700',
       style: 'normal',
     },
   ],
