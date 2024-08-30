e63d4d703815ef10f69d9b3e4b98228a8a7c7539
Zane <github@zane.sh>
2024-08-29 18:49:07 -0700

    Custom breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,11 +13,15 @@ const config = {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-        '3xl': '1536px',
-        '4xl': '1920px',
-      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4xl': '1920px',
     },
     extend: {
       colors: {
