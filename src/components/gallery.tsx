93a28a24a493618f8e6f1c20dfda5b4edd888f63
Zane <github@zane.sh>
2024-08-29 18:32:30 -0700

    Masonry version 0

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, Dispatch, SetStateAction } from 'react'
 import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import images from '@/lib/art.json'
@@ -21,39 +21,67 @@ const slides = images.map(({ filename, width, height, alt }) => ({
 }))
 
 export default function Gallery() {
-  const [open, setOpen] = useState<boolean>()
-  const [index, setIndex] = useState<number>(0)
+  const [openIndex, setOpenIndex] = useState<undefined | number>(undefined)
+
+  const chunk = (arr: any[], size: number) =>
+    Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+      arr.slice(i * size, i * size + size)
+    )
+
+  const SIZE = 3
+  const columns = chunk(images, SIZE)
 
   return (
     <section>
-      {open !== undefined && (
+      {openIndex !== undefined && (
         <Lightbox
-          open={open}
-          close={() => setOpen(false)}
+          open
+          close={() => setOpenIndex(undefined)}
           slides={slides}
-          index={index}
+          index={openIndex}
           controller={{
             closeOnBackdropClick: true,
           }}
         />
       )}
-      <div className="grid p-4 py-4 sm:pl-0 md:px-4 gap-4 items-start grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
-        {images.map(({ filename, alt }, index) => (
-          <Image
-            src={`/art/${filename}`}
-            className="rounded-lg object-cover cursor-pointer hover:shadow-xl transition-shadow duration-300 ease-in-out"
-            width={400}
-            height={400}
-            alt={alt}
-            key={index}
-            priority={index < 3}
-            onClick={() => {
-              setIndex(index)
-              setOpen(true)
-            }}
-          />
+      <div className="grid p-4 py-4 sm:pl-0 md:px-4 gap-4 items-start grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4">
+        {columns.map((column, i) => (
+          <div className="grid gap-4" key={i}>
+            {column.map(({ filename, alt }, index) => (
+              <GalleryImage
+                filename={filename}
+                alt={alt}
+                index={index + i * SIZE}
+                key={index}
+                setOpenIndex={setOpenIndex}
+              />
+            ))}
+          </div>
         ))}
       </div>
     </section>
   )
 }
+
+const GalleryImage = ({
+  filename,
+  alt,
+  index,
+  setOpenIndex,
+}: {
+  filename: string
+  alt: string
+  index: number
+  setOpenIndex: Dispatch<SetStateAction<number | undefined>>
+}) => (
+  <Image
+    src={require(`@/public/art/${filename}`).default}
+    className="rounded-lg object-cover h-auto w-full cursor-pointer hover:shadow-xl transition-shadow duration-300 ease-in-out"
+    placeholder="blur"
+    alt={alt}
+    priority={index < 3}
+    onClick={() => {
+      setOpenIndex(index)
+    }}
+  />
+)
