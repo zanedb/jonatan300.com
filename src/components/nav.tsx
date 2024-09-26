c44ae7681f354b2d859c53e4a011423890b51cea
Zane <github@zane.sh>
2024-09-25 17:30:50 -0700

    Fix image

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
 import { ThemeImage } from './theme-image'
+import Image from 'next/image'
 
 export default function Nav() {
   return (
     <nav className="flex justify-between items-center text-md py-4">
       <Link href="/">
         <ThemeImage
-          srcLight="/nameplate.png"
-          srcDark="/nameplate-dark.png"
-          alt={'“Jonatan” nametag with some swag'}
-          width={72}
-          height={54}
+          srcLight="/jonatan-light.png"
+          srcDark="/jonatan-dark.png"
+          alt={'“Jonatan” nametag'}
+          width={144}
+          height={108}
         />
       </Link>
     </nav>
